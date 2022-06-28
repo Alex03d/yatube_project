92f28feb64b8a5ba2bdd77750184e1e20a52a5d6
Alexander Dondokov <adondokov@gmail.com>
2022-06-28 17:09:01 +0300

    Commit from 3rd sprint and 5th lesson

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
+
 urlpatterns = [
-    path('group/<slug:slug>/', views.group_posts),
-    path('', views.index),
+    path('group_list/', views.group_list, name='group_list'),
+    # path('group/<slug:slug>/', views.group_list),
+    path('', views.index, name='index'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,33 @@ from django.shortcuts import render
 
 
 def index(request):
+    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
     template = 'posts/index.html'
-    return render(request, template)
+    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
+    title = 'Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Это главная страница проекта Yatube',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
 
-def group_posts(request, slug):
-    return HttpResponse('Типо посты группы')
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    title = 'Yatube'
+    context = {
+        # В словарь можно передать переменную
+        'title': title,
+        # А можно сразу записать значение в словарь. Но обычно так не делают
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    # Третьим параметром передаём словарь context
+    return render(request, template, context)
+
+
+# def group_posts(request, slug):
+#     return HttpResponse('Типо посты группы')
