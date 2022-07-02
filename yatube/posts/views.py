34380fff7b71527a84f355ee46714d051e2f5418
Alexander Dondokov <adondokov@gmail.com>
2022-07-02 17:58:17 +0300

    commit dd July 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,33 +3,53 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+# posts/views.py
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
+
 def index(request):
-    # Адрес шаблона сохраним в переменную, это не обязательно, но удобно
-    template = 'posts/index.html'
-    # Строку, которую надо вывести на страницу, тоже сохраним в переменную
-    title = 'Yatube'
-    # Словарь с данными принято называть context
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
-    # Третьим параметром передаём словарь context
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
-def group_list(request):
-    template = 'posts/group_list.html'
-    title = 'Yatube'
+# def group_list(request):
+#     template = 'posts/group_list.html'
+#     title = 'Yatube'
+#     context = {
+#         # В словарь можно передать переменную
+#         'title': title,
+#         # А можно сразу записать значение в словарь. Но обычно так не делают
+#         'text': 'Здесь будет информация о группах проекта Yatube',
+#     }
+#     # Третьим параметром передаём словарь context
+#     return render(request, template, context)
+
+
+# View-функция для страницы сообщества:
+def group_list(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        # В словарь можно передать переменную
-        'title': title,
-        # А можно сразу записать значение в словарь. Но обычно так не делают
-        'text': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'posts': posts,
     }
-    # Третьим параметром передаём словарь context
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
 
 
 # def group_posts(request, slug):
